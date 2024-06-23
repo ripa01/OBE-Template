1613f7cd023f8c669a8ebb609b6998dcf2371bf1
ripa01 <ripabiswas2739@gmail.com>
2024-06-24 00:05:26 +0600

    Updated navbar design

--- src/components/Navbar02.js
+++ src/components/Navbar02.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar02() {
   const [isOpen, setIsOpen] = useState(false);
@@ -8,12 +9,12 @@ export default function Navbar02() {
   };
 
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+    <nav className="sticky top-0 bg-indigo-100 border-gray-200 dark:bg-gray-900 dark:border-gray-700">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">OBE Master</span>
-        </a>
+        </Link>
         <button
           onClick={handleToggle}
           className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -32,16 +33,18 @@ export default function Navbar02() {
           </svg>
         </button>
         <div className={`w-full md:block md:w-auto ${isOpen ? 'block' : 'hidden'}`} id="navbar-dropdown">
-        <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-3 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          
+        <ul className="flex flex-col font-medium p-4 md:p-0 mt-4  rounded-lg bg-indigo-100 md:space-x-3 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-indigo-100 dark:bg-gray-800 md:dark:bg-gray-900">
+            
             <li>
-              <a href="/" className="inline-block py-2 w-full px-4 mt-1 text-md font-medium text-blue-700  bg-gray-50 rounded-lg hover:bg-blue-300 focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 dark:bg-blue-200 dark:hover:bg-blue-300">
+              <Link to="/login" className="inline-block py-2 w-full px-4 mt-1 text-md font-medium text-blue-700  bg-gray-50 rounded-lg hover:bg-blue-300 focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 dark:bg-blue-200 dark:hover:bg-blue-300">
                 Sign In
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="/" className="inline-block w-full py-2 px-4 mt-1 text-md font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 dark:bg-blue-600 dark:hover:bg-blue-700">
+              <Link to="/register" className="inline-block w-full py-2 px-4 mt-1 text-md font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 dark:bg-blue-600 dark:hover:bg-blue-700">
                 Sign Up
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
