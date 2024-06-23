7d65b1a53babaff0e2a80edda352137e5b7c8110
ripa01 <ripabiswas2739@gmail.com>
2024-06-24 00:04:18 +0600

    install flowbite for tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,54 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js'
+
+    
 
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+        fontFamily: {
+            hind: ["Hind Siliguri", "sans-serif"],
+            poppins: ["Poppins", "sans-serif"],
+        },
+        colors: ({ colors }) => ({
+            inherit: colors.inherit,
+            current: colors.current,
+            transparent: colors.transparent,
+            primary: "#6B5AE0",
+            secondary: "#887ED7",
+            tertiary: "#FF8FA2",
+            black: colors.black,
+            white: colors.white,
+            slate: colors.slate,
+            gray: colors.gray,
+            zinc: colors.zinc,
+            neutral: colors.neutral,
+            stone: colors.stone,
+            red: colors.red,
+            orange: colors.orange,
+            amber: colors.amber,
+            yellow: colors.yellow,
+            lime: colors.lime,
+            green: colors.green,
+            emerald: colors.emerald,
+            teal: colors.teal,
+            cyan: colors.cyan,
+            sky: colors.sky,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            violet: colors.violet,
+            purple: colors.purple,
+            fuchsia: colors.fuchsia,
+            pink: colors.pink,
+            rose: colors.rose,
+        }),
+    },
+},
+  plugins: [
+    require('flowbite/plugin')
+  ],
+
 }
 
