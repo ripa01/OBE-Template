22a91958fd0026a97569195c4b8c6f813841638f
ripa01 <ripabiswas2739@gmail.com>
2024-06-24 00:15:49 +0600

    update home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,51 +1,59 @@
 import React from 'react';
 import logo from '/Users/ripabiswas/GitHub Folders/OBE-Template/src/image/icons8-google-classroom-304.png'
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center w-full bg-gray-100">
-      <h1 className="text-4xl font-bold mb-8 mt-10 text-blue-700 text-center">Assigned Courses List</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        <div className="card w-96 bg-base-100 shadow-xl">
-          <figure className="px-10 pt-10">
-            <img src={logo} alt='class' className="rounded-xl" />
-          </figure>
-          <div className="card-body items-center text-center">
-            <h2 className="card-title">Software Engineering</h2>
-            <div className="card-actions">
-              <a href="/" className="inline-flex items-center py-2.5 px-4 mt-3 mb-3 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
-                View Details
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="card w-96 bg-base-100 shadow-xl">
-          <figure className="px-10 pt-10">
-          <img src={logo} alt="classroom" className="rounded-xl" />
-          </figure>
-          <div className="card-body items-center text-center">
-            <h2 className="card-title">Machine Learning</h2>
-            <div className="card-actions">
-              <button className="inline-flex items-center py-2.5 px-4 mt-3 mb-3 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
-                View Details
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="card w-96 bg-base-100 shadow-xl">
-          <figure className="px-10 pt-10">
-          <img src={logo} alt="classroom" className="rounded-xl" />
-          </figure>
-          <div className="card-body items-center text-center">
-            <h2 className="card-title">System Analysis & Design</h2>
-            <div className="card-actions">
-              <button className="inline-flex items-center py-2.5 px-4 mt-3 mb-3 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
-                View Details
-              </button>
-            </div>
-          </div>
+      <div className="bg-white py-10 px-10 sm:py-10 sm:px-10">
+      <div className="max-w-2xl mx-auto">
+        <h2 className="text-3xl mb-5 font-bold text-center tracking-tight text-blue-500 sm:text-4xl">
+          Assigned Course
+        </h2>
+      </div>
+     
+      <div className="container mx-auto mt-11 px-6 lg:px-8">
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          <HomeCard
+            img={logo}
+            title="Machine Learning"
+            subtitle="CSE 420"
+            link="/view"
+          />
+          <HomeCard
+            img={logo}
+            title="Algorithm"
+            subtitle="CSE 320"
+            link="/view"
+          />
+          <HomeCard
+            img={logo}
+            title="System Analysis"
+            subtitle="CSE 305"
+            link="/view"
+          />
         </div>
       </div>
+
     </div>
   );
 }
+
+const HomeCard = ({ img, title, subtitle, link }) => (
+  <div className="max-w-sm mx-auto bg-indigo-100 border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
+    <img className="w-full h-38 object-cover rounded-t-lg px-5 py-5" src={img} alt="card_img" />
+
+    <div className="p-5">
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+      <p className="mb-4 font-normal text-gray-700 dark:text-gray-400">{subtitle}</p>
+      <Link
+        to={link}
+        className="inline-flex items-center px-4 py-2 text-md font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      >
+        View Details
+        <svg className="w-4 h-4 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+        </svg>
+      </Link>
+    </div>
+  </div>
+)
